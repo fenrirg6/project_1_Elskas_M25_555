2efe5fb6f0386a1b46e35ea45635092161268c74
fenrirg6 <rbhbk2@gmail.com>
2025-10-03 21:32:38 +0300

    Commit 2.1: 1. Created constants.py, player_actions.py, utils.py files, 2. Added data to the constant.py file, added 4 custom rooms, 3. Imported ROOMS variable in the main.py file, 4. Added game_state variable to the main.py file.

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,4 +1,14 @@
 #!/usr/bin/venv python3
 
+from labyrinth_game.constants import ROOMS # import of room constants
+
+game_state = {
+    'player_inventory': [],  # Инвентарь игрока
+    'current_room': 'entrance',  # Текущая комната
+    'game_over': False,  # Значения окончания игры
+    'steps_taken': 0  # Количество шагов
+}
+
+
 def main():
     print("Первая попытка запустить проект!")
