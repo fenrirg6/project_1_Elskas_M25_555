b4bef0d1b013a0a771c30c2afe0e4f86dc375cc3
fenrirg6 <rbhbk2@gmail.com>
2025-10-03 22:44:41 +0300

    Commit 2.3: 1. Added move_player function to player_actions.py, 2. Added take_item function to player_actions.py, 3. Added process_comand to main.py, 4. Added use_item function to player_actions.py, 5. Added process_command to the main game loop.

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -2,22 +2,40 @@
 
 from labyrinth_game.constants import ROOMS # import of room constants
 from labyrinth_game.utils import describe_current_room
-from labyrinth_game.player_actions import get_input
+from labyrinth_game.player_actions import get_input, move_player, take_item, use_item
 
-game_state = {
-    'player_inventory': [],  # Инвентарь игрока
-    'current_room': 'entrance',  # Текущая комната
-    'game_over': False,  # Значения окончания игры
-    'steps_taken': 0  # Количество шагов
-}
+def process_command(game_state, command):
+    user_input = command.split()
 
+    match user_input[0]:
+        case 'look':
+            describe_current_room(game_state)
+        case 'go':
+            direction = user_input[1]
+            move_player(game_state, direction)
+        case 'take':
+            item_name = user_input[1]
+            take_item(game_state, item_name)
+        case 'use':
+            item_to_use = user_input[1]
+            use_item(game_state, item_to_use)
+        case 'smth else':
+            pass
 
 def main():
+    # initializing game state dict
+    game_state = {
+        'player_inventory': [],  # Инвентарь игрока
+        'current_room': 'entrance',  # Текущая комната
+        'game_over': False,  # Значения окончания игры
+        'steps_taken': 0  # Количество шагов
+    }
     # starting message
     print("Добро пожаловать в Лабиринт сокровищ!")
 
-    # describe current room
+    # describe starting room
     describe_current_room(game_state)
 
     while True:
-        player_input = get_input()
+        player_input = get_input("\nЧто вы хотите сделать? >")
+        process_command(game_state, player_input)
--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -1,5 +1,8 @@
+from labyrinth_game.constants import ROOMS
+from labyrinth_game.utils import describe_current_room
+
 def show_inventory(game_state):
-    inventory = game_state['inventory']
+    inventory = game_state["inventory"]
 
     if len(inventory) == 0:
         print("\nВаш инвентарь пуст.")
@@ -8,9 +11,62 @@ def show_inventory(game_state):
         for item in inventory:
             print(f"  - {item}")
 
-def get_input(prompt=">"):
+def get_input(prompt = ">"):
     try:
-        pass
+        user_input = input(prompt)
+        return user_input.strip().lower()
     except (KeyboardInterrupt, EOFError):
         print("\nВыход из игры.")
         return "quit"
+
+def move_player(game_state, direction):
+    """
+    movement function
+    """
+    current_room_name = game_state["current_room"]
+    current_room = ROOMS[current_room_name]
+    exits = current_room["exits"]
+
+    if direction in exits:
+        new_room = exits[direction]
+        game_state["current_room"] = new_room
+        game_state["steps_taken"] += 1
+
+        print(f"\nВы идете на {direction}...")
+        describe_current_room(game_state)
+    else:
+        print("Нельзя пойти в этом направлении.")
+
+def take_item(game_state, item_name):
+    current_room_name = game_state["current_room"]
+    current_room = ROOMS[current_room_name]
+    items_in_room = current_room["items"]
+
+    if item_name in items_in_room:
+        items_in_room.remove(item_name)
+        game_state["player_inventory"].append(item_name)
+        ROOMS[current_room_name]["items"].remove(item_name)
+        print(f"Вы подняли: {item_name}")
+    else:
+        print("Такого предмета здесь нет.")
+
+def use_item(game_state, item_name):
+    inventory = game_state["inventory"]
+
+    if item_name in inventory:
+        match item_name:
+            case "sword":
+                print("Стало немножко светлее...")
+            case "torch":
+                print("Вы чувствуете себя намного уверенее...")
+            case "bronze_box":
+                print("Вы открываете бронзовую шкатулку...")
+                if "rusty_key" not in inventory:
+                    print("Внутри нее оказался ржавый ключ!")
+                    inventory.append("rusty_key")
+                else:
+                    print("Внутри шкатулки ничего не оказалось...")
+            case _:
+                print(f"Вы не знаете, как использовать {item_name}")
+    else:
+        print("У вас нет такого предмета.")
